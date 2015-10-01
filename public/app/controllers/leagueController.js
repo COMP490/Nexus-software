9e4dc623b8ba1675889b13bfd080f00ec4067f15
BenDluzak <ben.dluzak@gmail.com>
2015-09-30 23:40:17 -0700

    code to add champs to LoL page

--- public/app/controllers/leagueController.js
+++ public/app/controllers/leagueController.js
@@ -1,13 +1,28 @@
+
 (function() {
-    
-    var leagueController = function ($scope) {
- 
+
+    var leagueController = function ($scope, $http) {
+
+var champListUrl = "https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=d22b06f5-db0b-4886-a43f-86ff2d96ee76"
+
         
+   $http.get(champListUrl).success( function(response) {
+      $scope.champions =  JSON.parse(response);
+       console.log($scope.champions);
+   $scope.freeChamps = []; 
       
-        
+          angular.forEach($scope.champions, function(champ){
+                  if(champ.freeToPlay === true){
+              
+                  $scope.freeChamps.push(champ.id);   
+                  }
+               })
+  
+            console.log($scope.freeChamps);
+   })
     };
     
-    leagueController.$inject = ['$scope'];
+    leagueController.$inject = ['$scope', '$http'];
 
     angular.module('nexusApp')
       .controller('leagueController', leagueController);
