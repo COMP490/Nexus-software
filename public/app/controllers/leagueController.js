4d6fca8455cba2a4047e393c529eeea545c1170b
BenDluzak <ben.dluzak@gmail.com>
2015-10-03 16:19:54 -0700

    added images and names for league controller

--- public/app/controllers/leagueController.js
+++ public/app/controllers/leagueController.js
@@ -10,21 +10,29 @@ var champListUrl = "https://na.api.pvp.net/api/lol/na/v1.2/champion?api_key=d22b
       $scope.champions =  response.champions;
        console.log($scope.champions);
    $scope.freeChamps = []; 
-   $scope.imageObject = [];   
+   $scope.imageObject = [];  
+       $scope.champData =[];
+       $scope.combinedObject = [];
           angular.forEach($scope.champions, function(champ){
                   if(champ.freeToPlay === true){
               
-                  $scope.freeChamps.push(champ.id); 
+                  $scope.freeChamps.push(champ); 
                  $http.get("https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/" + champ.id+ "?champData=image&api_key=d22b06f5-db0b-4886-a43f-86ff2d96ee76").success( function(data) {
-                     $scope.imageObject.push("http://ddragon.leagueoflegends.com/cdn/5.19.1/img/champion/" + data.image.full);
+                 
+                     $scope.champData.push(data);
                  })
+                 
                   }
               
                })
+          $scope.combinedObject.push($scope.champData);
+       
+          
   
-            console.log($scope.freeChamps);
-            console.log($scope.imageObject);
+            console.log( $scope.combinedObject);
+     
    })
+   
     };
     
     leagueController.$inject = ['$scope', '$http'];
