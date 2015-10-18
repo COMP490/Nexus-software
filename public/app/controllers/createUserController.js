1b63ddba4d301690da378b767d42b40625a9b66d
BenDluzak <ben.dluzak@gmail.com>
2015-10-18 13:54:50 -0700

    http request for create

--- public/app/controllers/createUserController.js
+++ public/app/controllers/createUserController.js
@@ -1,6 +1,6 @@
 (function () {
 
-    var createUserController = function ($scope) {
+    var createUserController = function ($scope , $http) {
         this.showModal = false;
 
                 this.showView = false;
@@ -24,10 +24,33 @@
                     this.counter++;
 
                 }
+                         $scope.signUp = function (credentials) {
+                    console.log(credentials)
+                    var userInfo = {
+
+                        "username": credentials.username,
+                        "password": credentials.password,
+                        "password2": credentials.password2
+
+                    };
+                    console.log(userInfo);
+
+                    $http.post('http://comp490.duckdns.org/create', userInfo).success(function (response) {
+                        $scope.response = response;
+                        console.log($scope.response.result);
+                        if ($scope.response.result == true) {
+                            $scope.loginStatus = true;
+                        } else {
+                            $scope.loginStatus = false;
+                            $scope.error = "The credientials entered were incorrect";
+                        }
+                        console.log($scope);
+                    })
+                };
 
             }
 
-    createUserController.$inject = ['$scope'];
+    createUserController.$inject = ['$scope', '$http'];
 
     angular.module('nexusApp')
         .controller('createUserController', createUserController);
